bb5e931cea5467bfd2d582fd2c4004f1c82922a6
Gabriel Konat <gabrielkonat@gmail.com>
2019-02-22 13:03:02 +0100

    Add additional configuration and plugins.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,18 @@ plugins {
   kotlin("jvm") version "1.3.20"
   `kotlin-dsl`
   `java-gradle-plugin`
-  publishing
   `maven-publish`
+}
+
+repositories {
+  gradlePluginPortal() // Add plugin portal as a repository, to be able to depend on Gradle plugins.
 }
 
 dependencies {
   compile("org.eclipse.jgit:org.eclipse.jgit:5.2.0.201812061821-r")
   // Compile-only dependencies for Gradle plugins that we need to use types from, but should still be applied/provided by users.
   compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.20")
+  compileOnly("org.gradle.kotlin:plugins:1.2.2")
 }
 
 kotlinDslPluginOptions {
@@ -36,9 +40,27 @@ gradlePlugin {
       id = "org.metaborg.gradle.config.java-application"
       implementationClass = "mb.gradle.config.JavaApplicationPlugin"
     }
+    create("metaborg-java-gradle-plugin") {
+      id = "org.metaborg.gradle.config.java-gradle-plugin"
+      implementationClass = "mb.gradle.config.JavaGradlePluginPlugin"
+    }
+
     create("metaborg-kotlin-library") {
       id = "org.metaborg.gradle.config.kotlin-library"
       implementationClass = "mb.gradle.config.KotlinLibraryPlugin"
+    }
+    create("metaborg-kotlin-application") {
+      id = "org.metaborg.gradle.config.kotlin-application"
+      implementationClass = "mb.gradle.config.KotlinApplicationPlugin"
+    }
+    create("metaborg-kotlin-gradle-plugin") {
+      id = "org.metaborg.gradle.config.kotlin-gradle-plugin"
+      implementationClass = "mb.gradle.config.KotlinGradlePluginPlugin"
+    }
+
+    create("metaborg-junit-testing") {
+      id = "org.metaborg.gradle.config.junit-testing"
+      implementationClass = "mb.gradle.config.JUnitTestingPlugin"
     }
 
     create("metaborg-devenv") {
