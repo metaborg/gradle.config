4b8e3b8ae15f7a63a3da1af83aaa43f421a9d976
Gabriel Konat <gabrielkonat@gmail.com>
2019-06-04 13:50:30 +0200

    Update JGit version. Update Gradle wrapper version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,8 @@ metaborg {
 }
 
 dependencies {
-  implementation("org.eclipse.jgit:org.eclipse.jgit:5.2.0.201812061821-r")
+  implementation("org.eclipse.jgit:org.eclipse.jgit:5.3.1.201904271842-r")
+  
   // Compile-only dependencies for Gradle plugins that we need to use types from, but should still be applied/provided by users.
   compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.20")
   compileOnly("org.gradle.kotlin:plugins:1.2.2")
