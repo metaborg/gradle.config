3f5474300d27411f9a88679f7f52327d37db5546
Gabriel Konat <gabrielkonat@gmail.com>
2019-06-04 13:59:19 +0200

    Add note about bootstrapping.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("org.metaborg.gradle.config.root-project") version "0.3.1" // Bootstrap with previous version.
-  id("org.metaborg.gradle.config.kotlin-gradle-plugin") version "0.3.1" // Bootstrap with previous version.
+  id("org.metaborg.gradle.config.root-project") version "0.3.1" // Bootstrap with version 0.3.1, higher does not seem to work.
+  id("org.metaborg.gradle.config.kotlin-gradle-plugin") version "0.3.1" // Bootstrap with version 0.3.1, higher does not seem to work.
   id("org.metaborg.gitonium") version "0.1.2"
   kotlin("jvm") version "1.3.21"
   `kotlin-dsl`
