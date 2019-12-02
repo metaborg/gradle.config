a3681ed018216d28c2cbf99b0729d50cc2385d94
Gabriel Konat <gabrielkonat@gmail.com>
2019-12-02 11:18:51 +0100

    Support newer versions of the Kotlin plugin that modifies the Kotlin library versions in afterEvaluate.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
   id("org.metaborg.gradle.config.root-project") version "0.3.1" // Bootstrap with version 0.3.1, higher does not seem to work.
   id("org.metaborg.gradle.config.kotlin-gradle-plugin") version "0.3.1" // Bootstrap with version 0.3.1, higher does not seem to work.
   id("org.metaborg.gitonium") version "0.1.2"
-  kotlin("jvm") version "1.3.21"
+  kotlin("jvm") version "1.3.41" // Stick with 1.3.41: Gradle 5.6.4's kotlin-dsl plugin uses it.
   `kotlin-dsl`
   `maven-publish`
 }
@@ -14,10 +14,10 @@ metaborg {
 
 dependencies {
   implementation("org.eclipse.jgit:org.eclipse.jgit:5.3.1.201904271842-r")
-  
+
   // Compile-only dependencies for Gradle plugins that we need to use types from, but should still be applied/provided by users.
-  compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.20")
-  compileOnly("org.gradle.kotlin:plugins:1.2.2")
+  compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
+  compileOnly("org.gradle.kotlin:plugins:1.3.2")
 }
 
 gradlePlugin {
