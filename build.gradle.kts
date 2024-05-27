c6493403f0932498a3b8a766ff55d156d3d7631d
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-05-27 13:36:54 +0200

    Reformat code to match Kotlin official style

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,90 +1,90 @@
 plugins {
-  id("org.metaborg.gradle.config.root-project") version "0.5.6"         // Bootstrap
-  id("org.metaborg.gradle.config.kotlin-gradle-plugin") version "0.5.6" // Bootstrap
-  id("org.metaborg.gitonium") version "1.2.0"
-  kotlin("jvm") version "1.3.41" // Stick with 1.3.41: Gradle 5.6.4's kotlin-dsl plugin uses it.
-  `kotlin-dsl`
-  `maven-publish`
+    id("org.metaborg.gradle.config.root-project") version "0.5.6"         // Bootstrap
+    id("org.metaborg.gradle.config.kotlin-gradle-plugin") version "0.5.6" // Bootstrap
+    id("org.metaborg.gitonium") version "1.2.0"
+    kotlin("jvm") version "1.3.41" // Stick with 1.3.41: Gradle 5.6.4's kotlin-dsl plugin uses it.
+    `kotlin-dsl`
+    `maven-publish`
 }
 
 metaborg {
-  kotlinApiVersion = "1.3"
-  kotlinLanguageVersion = "1.3"
+    kotlinApiVersion = "1.3"
+    kotlinLanguageVersion = "1.3"
 }
 
 repositories {
-  mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-  implementation("org.eclipse.jgit:org.eclipse.jgit:5.10.0.202012080955-r")
+    implementation("org.eclipse.jgit:org.eclipse.jgit:5.10.0.202012080955-r")
 
-  // Compile-only dependencies for Gradle plugins that we need to use types from, but should still be applied/provided by users.
-  compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
-  compileOnly("org.gradle.kotlin:plugins:1.3.2")
+    // Compile-only dependencies for Gradle plugins that we need to use types from, but should still be applied/provided by users.
+    compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
+    compileOnly("org.gradle.kotlin:plugins:1.3.2")
 
-  testImplementation("io.kotest:kotest-runner-junit5:4.2.0")
-  testImplementation("io.kotest:kotest-assertions-core:4.2.0")
-  testImplementation(gradleTestKit())
+    testImplementation("io.kotest:kotest-runner-junit5:4.2.0")
+    testImplementation("io.kotest:kotest-assertions-core:4.2.0")
+    testImplementation(gradleTestKit())
 }
 
 gradlePlugin {
-  plugins {
-    create("metaborg-root-project") {
-      id = "org.metaborg.gradle.config.root-project"
-      implementationClass = "mb.gradle.config.RootProjectPlugin"
-    }
-    create("metaborg-sub-project") {
-      id = "org.metaborg.gradle.config.sub-project"
-      implementationClass = "mb.gradle.config.SubProjectPlugin"
-    }
+    plugins {
+        create("metaborg-root-project") {
+            id = "org.metaborg.gradle.config.root-project"
+            implementationClass = "mb.gradle.config.RootProjectPlugin"
+        }
+        create("metaborg-sub-project") {
+            id = "org.metaborg.gradle.config.sub-project"
+            implementationClass = "mb.gradle.config.SubProjectPlugin"
+        }
 
-    create("metaborg-java-library") {
-      id = "org.metaborg.gradle.config.java-library"
-      implementationClass = "mb.gradle.config.JavaLibraryPlugin"
-    }
-    create("metaborg-java-application") {
-      id = "org.metaborg.gradle.config.java-application"
-      implementationClass = "mb.gradle.config.JavaApplicationPlugin"
-    }
-    create("metaborg-java-gradle-plugin") {
-      id = "org.metaborg.gradle.config.java-gradle-plugin"
-      implementationClass = "mb.gradle.config.JavaGradlePluginPlugin"
-    }
+        create("metaborg-java-library") {
+            id = "org.metaborg.gradle.config.java-library"
+            implementationClass = "mb.gradle.config.JavaLibraryPlugin"
+        }
+        create("metaborg-java-application") {
+            id = "org.metaborg.gradle.config.java-application"
+            implementationClass = "mb.gradle.config.JavaApplicationPlugin"
+        }
+        create("metaborg-java-gradle-plugin") {
+            id = "org.metaborg.gradle.config.java-gradle-plugin"
+            implementationClass = "mb.gradle.config.JavaGradlePluginPlugin"
+        }
 
-    create("metaborg-kotlin-library") {
-      id = "org.metaborg.gradle.config.kotlin-library"
-      implementationClass = "mb.gradle.config.KotlinLibraryPlugin"
-    }
-    create("metaborg-kotlin-application") {
-      id = "org.metaborg.gradle.config.kotlin-application"
-      implementationClass = "mb.gradle.config.KotlinApplicationPlugin"
-    }
-    create("metaborg-kotlin-testing-only") {
-      id = "org.metaborg.gradle.config.kotlin-testing-only"
-      implementationClass = "mb.gradle.config.KotlinTestingOnlyPlugin"
-    }
-    create("metaborg-kotlin-gradle-plugin") {
-      id = "org.metaborg.gradle.config.kotlin-gradle-plugin"
-      implementationClass = "mb.gradle.config.KotlinGradlePluginPlugin"
-    }
+        create("metaborg-kotlin-library") {
+            id = "org.metaborg.gradle.config.kotlin-library"
+            implementationClass = "mb.gradle.config.KotlinLibraryPlugin"
+        }
+        create("metaborg-kotlin-application") {
+            id = "org.metaborg.gradle.config.kotlin-application"
+            implementationClass = "mb.gradle.config.KotlinApplicationPlugin"
+        }
+        create("metaborg-kotlin-testing-only") {
+            id = "org.metaborg.gradle.config.kotlin-testing-only"
+            implementationClass = "mb.gradle.config.KotlinTestingOnlyPlugin"
+        }
+        create("metaborg-kotlin-gradle-plugin") {
+            id = "org.metaborg.gradle.config.kotlin-gradle-plugin"
+            implementationClass = "mb.gradle.config.KotlinGradlePluginPlugin"
+        }
 
-    create("metaborg-junit-testing") {
-      id = "org.metaborg.gradle.config.junit-testing"
-      implementationClass = "mb.gradle.config.JUnitTestingPlugin"
-    }
+        create("metaborg-junit-testing") {
+            id = "org.metaborg.gradle.config.junit-testing"
+            implementationClass = "mb.gradle.config.JUnitTestingPlugin"
+        }
 
-    create("metaborg-devenv-settings") {
-      id = "org.metaborg.gradle.config.devenv-settings"
-      implementationClass = "mb.gradle.config.devenv.DevenvSettingsPlugin"
+        create("metaborg-devenv-settings") {
+            id = "org.metaborg.gradle.config.devenv-settings"
+            implementationClass = "mb.gradle.config.devenv.DevenvSettingsPlugin"
+        }
+        create("metaborg-devenv-repositories") {
+            id = "org.metaborg.gradle.config.devenv-repositories"
+            implementationClass = "mb.gradle.config.devenv.DevenvRepositoriesPlugin"
+        }
     }
-    create("metaborg-devenv-repositories") {
-      id = "org.metaborg.gradle.config.devenv-repositories"
-      implementationClass = "mb.gradle.config.devenv.DevenvRepositoriesPlugin"
-    }
-  }
 }
 
 tasks.withType<Test>().configureEach {
-  useJUnitPlatform()
+    useJUnitPlatform()
 }
