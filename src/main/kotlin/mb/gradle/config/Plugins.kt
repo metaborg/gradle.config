bce9224da8e88930f58842a9428d830ae6b69541
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-17 02:50:47 +0200

    Set Java version to 11

--- src/main/kotlin/mb/gradle/config/Plugins.kt
+++ src/main/kotlin/mb/gradle/config/Plugins.kt
@@ -12,7 +12,7 @@ open class MetaborgExtension(private val project: Project) {
     }
 
 
-    var javaVersion = JavaVersion.VERSION_17
+    var javaVersion = JavaVersion.VERSION_11
     var javaCreatePublication = true
     var javaCreateSourcesJar = true
     var javaPublishSourcesJar = true
